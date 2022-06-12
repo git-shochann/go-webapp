afe84356839b6ec3c1daf2b20142cc1274b5b72c
git-shochann <sho_825.sa216@docomo.ne.jp>
2022-06-12 19:37:15 +0900

    complete to get todo func

--- app/models/todos.go
+++ app/models/todos.go
@@ -1,6 +1,7 @@
 package models
 
 import (
+	"database/sql"
 	"log"
 	"time"
 )
@@ -13,13 +14,29 @@ type Todo struct {
 }
 
 func (u User) CreateTodo(content string) (err error) {
-	CreateTodoCmd := `insert into todos (
+	createTodoCmd := `insert into todos (
 		content,
 		user_id,
 		created_at) values (?, ?, ?)`
-	_, err = Db.Exec(CreateTodoCmd, content, u.ID, time.Now())
+	_, err = Db.Exec(createTodoCmd, content, u.ID, time.Now())
 	if err != nil {
 		log.Fatalln(err)
 	}
 	return err
+}
+
+func GetTodo(id int) (todo Todo, err error) {
+	getTodoCmd := `select id, content, user_id, created_at from todos where id = ?`
+	todo = Todo{}
+	row := Db.QueryRow(getTodoCmd, id)
+	err = row.Scan(&todo.ID, &todo.Content, &todo.UserID, &todo.CreatedAt)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			log.Fatalf("type is %T, value is %v\n", err, err) // ここの型とメッセージを調べてみた
+		} else {
+			log.Fatalln(err)
+		}
+	}
+
+	return todo, err
 }
--- main.go
+++ main.go
@@ -1,68 +1,66 @@
 package main
 
-import (
-	"fmt"
-	"go-webapp/app/models"
-	"go-webapp/config"
-)
-
 func main() {
 
-	fmt.Println("##########################")
+	// fmt.Println("##########################")
 
-	fmt.Println("Configを確認します")
+	// fmt.Println("Configを確認します")
 
-	fmt.Println(config.Config.Port)
-	fmt.Println(config.Config.SQLDriver)
-	fmt.Println(config.Config.DbName)
-	fmt.Println(config.Config.LogFile)
+	// fmt.Println(config.Config.Port)
+	// fmt.Println(config.Config.SQLDriver)
+	// fmt.Println(config.Config.DbName)
+	// fmt.Println(config.Config.LogFile)
 
-	fmt.Println("##########################")
+	// fmt.Println("##########################")
 
-	fmt.Println("Userを確認します")
+	// fmt.Println("Userを確認します")
 
-	// init関数を呼び出すためだけに書いたコード
-	fmt.Println(models.Db)
+	// // init関数を呼び出すためだけに書いたコード
+	// fmt.Println(models.Db)
 
-	// 構造体の初期化
+	// // 構造体の初期化
 
-	// u := &models.User{
-	// 	Name:     "Sho",
-	// 	Email:    "aaa@gmail.com",
-	// 	PassWord: "test000",
-	// }
+	// // u := &models.User{
+	// // 	Name:     "Sho",
+	// // 	Email:    "aaa@gmail.com",
+	// // 	PassWord: "test000",
+	// // }
+	// // fmt.Println(u)
+	// // u.CreateUser()
+
+	// // Userを取得する
+	// u, err := models.GetUser(1)
+	// fmt.Println(u, err)
+
+	// // Userを更新する
+	// // 以下のようにしてUser型のuを上書きしてから、レシーバ.メソッドで実行する
+	// u.Name = "Yua"
+	// u.Email = "bbb@gmail.com"
+	// u.UpdateUser()
+
+	// // 再度表示
+	// u, _ = models.GetUser(1)
 	// fmt.Println(u)
-	// u.CreateUser()
 
-	// Userを取得する
-	u, err := models.GetUser(1)
-	fmt.Println(u, err)
+	// // Userの削除
+	// u.DeleteUser()
 
-	// Userを更新する
-	// 以下のようにしてUser型のuを上書きしてから、レシーバ.メソッドで実行する
-	u.Name = "Yua"
-	u.Email = "bbb@gmail.com"
-	u.UpdateUser()
+	// // 再度表示 -> 初期値のUser構造体が返ってくる
+	// u, _ = models.GetUser(2)
+	// fmt.Println(u)
 
-	// 再度表示
-	u, _ = models.GetUser(1)
-	fmt.Println(u)
+	// fmt.Println("##########################")
 
-	// Userの削除
-	u.DeleteUser()
+	// fmt.Println("Todoを確認します")
 
-	// 再度表示 -> 初期値のUser構造体が返ってくる
-	u, _ = models.GetUser(2)
-	fmt.Println(u)
+	// // Todoの作成
+	// fmt.Println(u)
+	// u.CreateTodo("旅行の計画を立てる")
 
-	fmt.Println("##########################")
+	// fmt.Println("##########################")
 
-	fmt.Println("Todoを確認します")
-
-	// Todoの作成
-	fmt.Println(u)
-	u.CreateTodo("旅行の計画を立てる")
-
-	fmt.Println("##########################")
+	// Todoの取得
+	// todo, _ := models.GetTodo(2)
+	// fmt.Println(todo)
 
 }
