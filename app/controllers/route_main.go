2f173968ed0c57239fb47ba518df4f8c3983ddad
git-shochann <sho_825.sa216@docomo.ne.jp>
2022-06-26 23:59:19 +0900

    complete signup

--- app/controllers/route_main.go
+++ app/controllers/route_main.go
@@ -1,12 +1,41 @@
 package controllers
 
 import (
+	"go-webapp/app/models"
+	"log"
 	"net/http"
+	"time"
 )
 
 // Topページに行った際のハンドラーを作る
 func top(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		// Helloを渡す？
+		generateHTML(w, nil, "layout", "public_navbar", "signup")
+	} else if r.Method == "POST" {
+		err := r.ParseForm()
+		if err != nil {
+			log.Fatalln(err)
+		}
+		user := models.User{
+			// name属性から取得
+			Name:     r.PostFormValue("name"),
+			Email:    r.PostFormValue("email"),
+			PassWord: r.PostFormValue("password"),
+		}
+		if err := user.CreateUser(); err != nil {
+			log.Panicln(err)
+		}
+		http.Redirect(w, r, "/", http.StatusFound)
+	}
 
-	// Helloを渡す？
-	generateHTML(w, "Hello!", "layout", "public_navbar", "top")
+}
+
+type User struct {
+	ID        int
+	UUID      string
+	Name      string
+	Email     string
+	PassWord  string
+	CreatedAt time.Time
 }
